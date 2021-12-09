bd2cd0e4b2ffc1ab888951f743b43435c49ab884
Michał Markiewicz <mmarkiewicz@gog.com>
2021-12-09 18:39:52 +0100

    mock doctors response

--- src/Adapters/SlotSupplier/ExternalSlot.php
+++ src/Adapters/SlotSupplier/ExternalSlot.php
@@ -3,9 +3,12 @@ declare(strict_types=1);
 
 namespace App\Adapters\SlotSupplier;
 
+use Webmozart\Assert\Assert;
+
 class ExternalSlot
 {
     public function __construct(public \DateTimeImmutable $from, public \DateTimeImmutable $to)
     {
+        Assert::true($from < $to);
     }
 }
